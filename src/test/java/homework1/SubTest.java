78899f249b78a3768e19680a540ee1b150d4af3e
Nata1ib <nataliboitsova@yandex.ru>
2023-05-24 15:43:21 +0300

    homework 1

---
+++ src/test/java/homework1/SubTest.java
@@ -0,0 +1,21 @@
+package homework1;
+
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.ValueSource;
+
+import org.junit.jupiter.api.Assertions;
+
+class SubTest extends CalculatorTest {
+    @ParameterizedTest
+    @CsvSource({"0, 0", "0, 91", "-78, 1123", "123, -897", "1254, 1254", "1237.21334, 1234645.231", "-123.231, -123.3221"})
+    void testSub(double first, double second) {
+        Assertions.assertEquals(first - second, calculator.sub(first, second), 0.00001);
+    }
+
+    @ParameterizedTest
+    @ValueSource(doubles = {0, Double.MAX_VALUE, Double.MIN_VALUE})
+    void testSubOnBoundaries(double first) {
+        Assertions.assertEquals(0, calculator.sub(first, first), 0.00001);
+    }
+}
